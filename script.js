84ab2a5c565b84bc44df0af33d92c4824313f285
Roshan <71066704+roshan194@users.noreply.github.com>
2025-03-06 11:21:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required
+it('testing modal close functionality', () => {
+    cy.get('button').first().click().then(() => {
+        cy.get('.modal').should('be.visible');
+
+        // Fix: Cypress should wait for the modal to close completely
+        cy.get('.close').click();
+        cy.wait(500);  // Ensures Bootstrap animation completes
+
+        // Instead of checking 'not.be.visible', check if the modal is hidden in the DOM
+        cy.get('.modal').should('have.attr', 'aria-hidden', 'true');
+    });
+});
